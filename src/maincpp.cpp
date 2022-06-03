6fcb304b2451d1e1a257785d405a44d1810df577
Gui Maciel Pereira <gjmacielpereira1@sheffield.ac.uk>
2022-06-03 14:11:49 +0100

    Add project files.

---
+++ src/maincpp.cpp
@@ -0,0 +1,194 @@
+#include "std_lib_facilities.h"
+
+// This exercise was actually incredibly helpful to demonstrate tokens and grammars
+// I am very impressed with it.
+
+
+class Token {
+public:
+	char kind;
+	double value;
+};
+
+class TokenStream {               // Class declarations appear first, and only then comes the definitions
+public:
+	// TokenStream();
+	void putBack(Token t);
+	Token get();
+
+private:
+	bool full{ false };
+	Token tokenAvailable;
+
+};
+
+
+void TokenStream::putBack(Token t) {
+	full = true;
+	tokenAvailable = t;
+}
+
+
+Token TokenStream::get() {
+
+	if (full) {             // If token already available, do not read from cin
+		full = false;
+		return tokenAvailable;
+	}
+
+	char ch;
+	cin >> ch;   // Read one character
+	switch (ch) {
+	
+	case '+': case '-': case '*': case '/': 
+	case '=': case '(': case ')': case 'x':
+	case '{': case '}': case '!':
+		return Token{ ch };
+	
+	case '0': case '1': case '2': case '3': case '4':      // Number beggins
+	case '5': case '6': case '7': case '8': case '9': case '.':
+		cin.putback(ch);
+		double value;
+		cin >> value;
+		return Token{ '8' , value};
+
+	default:
+		error("Token not recognized.");
+	}
+
+}
+
+
+// Write Grammar
+
+TokenStream ts;
+double expression();     // Declarations allows tells the compiler to trust that this function is defined somewhere
+double term();
+double secondary();
+double primary();
+
+
+double expression() {
+
+	double buffer = term();
+	Token t = ts.get();
+
+	while (true) {
+		switch (t.kind) {
+		case '+':
+			buffer += term();
+			t = ts.get();            // Initially forgot to update the loop
+			break;
+		case '-':
+			buffer -= term();
+			t = ts.get();
+			break;
+		default:
+			ts.putBack(t);
+			return buffer;
+		}
+	}
+}
+
+
+double term() {
+
+	double buffer = secondary();
+	Token t = ts.get();
+
+	while (true) {
+		switch (t.kind) {
+		case '*':
+			buffer *= secondary();
+			t = ts.get();
+			break;
+		case '/':
+		{
+			double d = secondary();
+			if (d == 0) error("Cannot divide by zero!");
+			buffer /= d;
+			t = ts.get();
+			break;
+		}
+		default:
+			ts.putBack(t);
+			return buffer;
+		}
+	}
+}
+
+
+double secondary() {
+	double buffer = primary();
+
+	Token t = ts.get();
+
+	if (t.kind == '!') {
+		int x = int(buffer);
+		int fact = 1;
+		for (int i = 1; i <= x; i++) fact *= i;
+		return fact;
+	}
+	else
+		ts.putBack(t);
+	return buffer;
+}
+
+
+double primary() {
+
+	Token t = ts.get();
+
+	switch (t.kind) {
+	case '(':
+	{                                  // Need curly brackets to initialize variable inside case
+		double d = expression(); 
+		if (ts.get().kind != ')') error("missing ')'");         // char ')' gets eaten here i.e. not returned to token_stream
+		return d;
+	}
+	case '{':
+	{                                  // Need curly brackets to initialize variable inside case
+		double d = expression();
+		if (ts.get().kind != '}') error("missing '}'");         // char ')' gets eaten here i.e. not returned to token_stream
+		return d;
+	}
+	case '8':
+		return t.value;
+
+		// Add statement that checkes if next char is '!' here and performs the factorial before returning.
+
+	default:
+		error("Bad token.");
+	}
+}
+
+int main()
+try {
+	cout << "Welcome to our simple calculator."
+		<< "\nPlease enter floating point numbers."
+		<< "\nExpressions available: +, -, *, /"
+		<< "\nPress '=' to return value and 'x' to quit."
+		<< "\nHave fun!" << "\n\n";
+
+	double val = 0;
+	while (cin) {
+		Token t = ts.get();
+
+		if (t.kind == 'x') break;
+		if (t.kind == '=') cout << "=" << val << '\n';
+		else ts.putBack(t);
+		
+		val = expression();    // Will be called in the next iteration
+	}
+	keep_window_open();
+}
+catch (exception& e) {
+	cerr << e.what() << '\n';
+	keep_window_open();
+	return 1;
+}
+catch (...) {
+	cerr << "exception \n";
+	keep_window_open();
+	return 2;
+}
